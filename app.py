47126573bcc7e38d1e26386c15f027d569af3089
Daniel <nieltansah@gmail.com>
2017-11-11 21:37:16 +0700

    Revert "Do not handle message"
    This reverts commit 4f2a69f5ce0400850aa32d0a34d83ecabb4055a1.

--- app.py
+++ app.py
@@ -37,7 +37,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  pass
+  line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
