4f2a69f5ce0400850aa32d0a34d83ecabb4055a1
Daniel <nieltansah@gmail.com>
2017-11-11 21:25:17 +0700

    Do not handle message

--- app.py
+++ app.py
@@ -37,9 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=event.message.text))
+  pass
 
 
 if __name__ == "__main__":
