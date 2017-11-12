6b115389a71f33cd754a3dca00864594830e8ffd
Oktavianus Handika <13515035@std.stei.itb.ac.id>
2017-11-13 06:01:46 +0700

    Removed comments in recommend handler

--- handler/recommend.py
+++ handler/recommend.py
@@ -5,31 +5,6 @@ from handler.compare import CompareHandler
 
 from linebot.models import *
 
-# class CompareHandler(Handler):
-
-#   def __init__(self, reply_token, bot_api):
-#     carousel_template = CarouselTemplate(columns=[
-#       CarouselColumn(text='Rp 25.000,-', title='Arabian Egg', actions=[
-#         PostbackTemplateAction(label='Choose', data='compare_ok')
-#       ]),
-#       CarouselColumn(text='Rp 25.000,-', title='Australian Egg', actions=[
-#         PostbackTemplateAction(label='Buy', data='buy_b'),
-#         PostbackTemplateAction(label='Details', data='details_b'),
-#         PostbackTemplateAction(label='Choose', data='compare_ok')
-#       ])
-#     ])
-#     template_message = TemplateSendMessage(
-#       alt_text='Carousel alt text', template=carousel_template)
-#     bot_api.reply_message(reply_token, template_message)
-
-#   def handle_postback(self, event, bot_api):
-#     data = event.postback.data
-
-#     if data == 'compare_ok':
-#       bot_api.reply_message(
-#         event.reply_token,
-#         TextMessage(text="Arabian egg vs Mysterious egg\n\nShell:\nSpike shell vs Smooth shell\n\nShape:\nRound shape vs Oval shape\n\nSize:10inch vs 18inch\n\nColor:\nRed vs Cream.\n\nMysterious egg's exclusive properties:\nDoes not break when thrown with a force.\nIs not known if it's an actual egg.\n\nAustralian egg's exclusive properties:\n-"))
-
 class RecommendByPromoHandler(Handler):
 
   def __init__(self, reply_token, bot_api):
