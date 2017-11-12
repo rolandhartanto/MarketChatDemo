9a4a1b78ad0a887a1dd3a1b38eb6a58cc26f4162
Daniel <nieltansah@gmail.com>
2017-11-12 22:09:10 +0700

    Add cancel backdoor

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 #  not use this file except in compliance with the License. You may obtain
 #  a copy of the License at
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -21,21 +21,21 @@ import sys
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler
+  LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+  InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-    SourceUser, SourceGroup, SourceRoom,
-    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
-    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URITemplateAction,
-    PostbackTemplateAction, DatetimePickerTemplateAction,
-    CarouselTemplate, CarouselColumn, PostbackEvent,
-    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-    ImageMessage, VideoMessage, AudioMessage, FileMessage,
-    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+  MessageEvent, TextMessage, TextSendMessage,
+  SourceUser, SourceGroup, SourceRoom,
+  TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+  ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URITemplateAction,
+  PostbackTemplateAction, DatetimePickerTemplateAction,
+  CarouselTemplate, CarouselColumn, PostbackEvent,
+  StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+  ImageMessage, VideoMessage, AudioMessage, FileMessage,
+  UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 
 from handler.default import DefaultHandler
@@ -46,54 +46,62 @@ app = Flask(__name__)
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
+  print('Specify LINE_CHANNEL_SECRET as environment variable.')
+  sys.exit(1)
 if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+  print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+  sys.exit(1)
 
 bot_api = LineBotApi(channel_access_token)
 webhook = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+  # get X-Line-Signature header value
+  signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+  # get request body as text
+  body = request.get_data(as_text=True)
+  app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        webhook.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+  # handle webhook body
+  try:
+    webhook.handle(body, signature)
+  except InvalidSignatureError:
+    abort(400)
 
-    return 'OK'
+  return 'OK'
 
 
 handle = DefaultHandler()
 
 @handle.handle_set_handler
 def set_handler(handler):
-    handle = handler
+  handle = handler
 
 @webhook.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    handle.handle_text(event, bot_api)
+  text = event.message.text
+
+  if text == 'cancel':
+    bot_api.reply_message(
+      reply_token,
+      TextMessage(text="Switch to main menu."))
+    handle.switch_handler(DefaultHandler())
+
+  handle.handle_text(event, bot_api)
 
 @webhook.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
-    handle.handle_location(event, bot_api)
+  handle.handle_location(event, bot_api)
 
 @webhook.add(PostbackEvent)
 def handle_postback(event):
-    handle.handle_postback(event, bot_api)
+  handle.handle_postback(event, bot_api)
 
 
 # Run application.
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
