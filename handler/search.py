c7074ea291b2dd93a3b2f2bf118e19287e8cb30d
Roland Hartanto <13515107@std.stei.itb.ac.id>
2017-11-13 05:11:34 +0700

    search test

--- handler/search.py
+++ handler/search.py
@@ -6,20 +6,19 @@ from linebot.models import *
 
 class GroceryHandler(Handler):
   def __init__(self, reply_token, bot_api):
-    image_carousel_template = ImageCarouselTemplate(columns=[
-      ImageCarouselColumn(
-        image_url='https://via.placeholder.com/1024x1024',
-        action=PostbackTemplateAction(label='Arabian egg\nRp 25.000,00', data='arabian-egg')),
-      ImageCarouselColumn(
-        image_url='https://via.placeholder.com/1024x1024',
-        action=PostbackTemplateAction(label='Australian egg\nRp 25.000,00', data='australian-egg'))
+    carousel_template = CarouselTemplate(columns=[
+      CarouselColumn(text='Rp 25.000,00', title='Arabian egg', actions=[
+        PostbackTemplateAction(label='view detail', data='arabian-egg')
+      ]),
+      CarouselColumn(text='Rp 25.000,00', title='Australian egg', actions=[
+        PostbackTemplateAction(label='view detail', data='arabian-egg')
+      ])
     ])
     template_message = TemplateSendMessage(
-      alt_text='ImageCarousel alt text', template=image_carousel_template)
+      alt_text='Carousel alt text', template=carousel_template)
     bot_api.reply_message(reply_token, template_message)
 
 class SearchHandler(Handler):
-
   def __init__(self, reply_token, bot_api):
     buttons_template = ButtonsTemplate(
       title='In what category?', text='Choose category:', actions=[
