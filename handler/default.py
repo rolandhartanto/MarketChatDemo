7ce5a3cbf2657b53898cb295a97d5b5daa12b03f
Daniel <nieltansah@gmail.com>
2017-11-12 21:49:05 +0700

    Use sample demo

--- handler/default.py
+++ handler/default.py
@@ -10,16 +10,22 @@ class DefaultHandler(Handler):
     self.set_handler_callback = cb
 
   def handle_text(self, event, bot_api):
-    text = event.message.text
+    text = event.message.text.lower()
 
     if text == 'hi':
-      buttons = ButtonsTemplate(
-        title='What do you want to do?', actions=[
-          PostbackTemplateAction(label='Search Items', data='search'),
-          PostbackTemplateAction(label='View transactions', data='transact')
+      buttons_template = ButtonsTemplate(
+        title='My buttons sample', text='Hello, my buttons', actions=[
+          URITemplateAction(
+            label='Go to line.me', uri='https://line.me'),
+          PostbackTemplateAction(label='ping', data='ping'),
+          PostbackTemplateAction(
+            label='ping with text', data='ping',
+            text='ping'),
+          MessageTemplateAction(label='Translate Rice', text='米')
         ])
-      message = TemplateSendMessage(alt_text='Actions', template=buttons)
-      bot_api.reply_message(event.reply_token, message)
+        template_message = TemplateSendMessage(
+          alt_text='Buttons alt text', template=buttons_template)
+        bot_api.reply_message(event.reply_token, template_message)
 
   def handle_postback(self, event, bot_api):
     data = event.postback.data
