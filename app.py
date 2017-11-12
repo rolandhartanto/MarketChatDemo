e410d496bf15003264ce093fa938bb2b9213b6c4
Daniel <nieltansah@gmail.com>
2017-11-12 22:24:13 +0700

    Debug

--- app.py
+++ app.py
@@ -77,13 +77,13 @@ handle = DefaultHandler()
 
 @handle.handle_set_handler
 def set_handler(handler):
-  print("SET CURRENT HANDLE: " + handle)
+  print("SET CURRENT HANDLE: " + str(handle))
   handle = handler
 
 @webhook.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
   text = event.message.text.lower()
-  print("SERVER: " + handle)
+  print("SERVER: " + str(handle))
 
   if text == 'cancel':
     bot_api.reply_message(
