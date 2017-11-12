03e24521d814c457223fc68bcaede53925c5fe6d
Daniel <nieltansah@gmail.com>
2017-11-13 01:31:06 +0700

    Search, compare

--- handler/search.py
+++ handler/search.py
@@ -1,27 +1,48 @@
 # default.py
 
-from . import Handler
+from handler import Handler
+from handler.compare import CompareHandler
 
 from linebot.models import *
 
 class GroceryHandler(Handler):
   def __init__(self, reply_token, bot_api):
     carousel_template = CarouselTemplate(columns=[
-      CarouselColumn(text='hoge1', title='fuga1', actions=[
-        PostbackTemplateAction(label='ping', data='ping'),
-        PostbackTemplateAction(label='ping', data='ping')
+      CarouselColumn(text='Rp 25.000,-', title='Arabian Egg', actions=[
+        PostbackTemplateAction(label='Buy', data='buy_a'),
+        PostbackTemplateAction(label='Details', data='details_a'),
+        PostbackTemplateAction(label='Compare', data='compare')
       ]),
-      CarouselColumn(text='hoge1', title='fuga1', actions=[
-        PostbackTemplateAction(label='ping', data='ping'),
-        PostbackTemplateAction(label='ping', data='ping')
+      CarouselColumn(text='Rp 25.000,-', title='Australian Egg', actions=[
+        PostbackTemplateAction(label='Buy', data='buy_b'),
+        PostbackTemplateAction(label='Details', data='details_b'),
+        PostbackTemplateAction(label='Compare', data='compare')
       ])
     ])
     template_message = TemplateSendMessage(
       alt_text='Carousel alt text', template=carousel_template)
     bot_api.reply_message(reply_token, template_message)
 
-class SearchHandler(Handler):
+  def handle_postback(self, event, bot_api):
+    data = event.postback.data
 
+    if data == 'buy_a':
+      # TODO: Checkout
+      pass
+    elif data == 'buy_b':
+      # TODO: Checkout
+      pass
+    elif data == 'details_a':
+      bot_api.reply_message(
+        event.reply_token, TextSendMessage(text="'Arabian egg\n\nPrice: Rp. 25,000.00\nStore location: Yogya Karapitan (Bandung)\nCondition: Good"))
+    elif data == 'details_b':
+      bot_api.reply_message(
+        event.reply_token, TextSendMessage(text="'Australian egg\n\nPrice: Rp. 25,000.00\nStore location: Yogya Riau Junction (Bandung)\nCondition: Good"))
+    elif data == 'compare':
+      self.switch_handler(CompareHandler(event.reply_token, bot_api))
+
+
+class SearchHandler(Handler):
   def __init__(self, reply_token, bot_api):
     buttons_template = ButtonsTemplate(
       title='In what category?', text='Choose category:', actions=[
