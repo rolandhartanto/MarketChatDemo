74071f904387ae3e60bf3c9ac39057cbe95c0b1c
Daniel <raditya1710@gmail.com>
2017-11-12 23:17:13 +0700

    Use 'self', again!

--- handler/search.py
+++ handler/search.py
@@ -6,16 +6,18 @@ from linebot.models import *
 
 class GroceryHandler(Handler):
   def __init__(self, reply_token, bot_api):
-    image_carousel_template = ImageCarouselTemplate(columns=[
-      ImageCarouselColumn(
-        image_url='https://via.placeholder.com/1024x1024',
-        action=PostbackTemplateAction(label='Arabian egg\nRp 25.000,00', data='arabian-egg')),
-      ImageCarouselColumn(
-        image_url='https://via.placeholder.com/1024x1024',
-        action=PostbackTemplateAction(label='Australian egg\nRp 25.000,00', data='australian-egg'))
+    carousel_template = CarouselTemplate(columns=[
+      CarouselColumn(text='hoge1', title='fuga1', actions=[
+        PostbackTemplateAction(label='ping', data='ping'),
+        PostbackTemplateAction(label='ping', data='ping')
+      ]),
+      CarouselColumn(text='hoge1', title='fuga1', actions=[
+        PostbackTemplateAction(label='ping', data='ping'),
+        PostbackTemplateAction(label='ping', data='ping')
+      ])
     ])
     template_message = TemplateSendMessage(
-      alt_text='ImageCarousel alt text', template=image_carousel_template)
+      alt_text='Carousel alt text', template=carousel_template)
     bot_api.reply_message(reply_token, template_message)
 
 class SearchHandler(Handler):
@@ -34,6 +36,6 @@ class SearchHandler(Handler):
     data = event.postback.data
 
     if data == 'grocery':
-      switch_handler(GroceryHandler(event.reply_token, bot_api))
+      self.switch_handler(GroceryHandler(event.reply_token, bot_api))
     elif data == 'fashion':
       pass
