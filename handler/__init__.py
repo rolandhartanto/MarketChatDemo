99b70293ecc464828306903489410349f27f0f52
Daniel <nieltansah@gmail.com>
2017-11-12 22:16:05 +0700

    Use self keyword

--- handler/__init__.py
+++ handler/__init__.py
@@ -4,7 +4,7 @@ class Handler:
   set_handler_callback = None
 
   def switch_handler(self, handler):
-    handler.set_handler_callback = set_handler_callback
+    handler.set_handler_callback = self.set_handler_callback
     self.set_handler_callback(handler)
 
   def handle_text(self, event, bot_api):
