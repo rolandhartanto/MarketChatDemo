b73d35c927ae24efc0e64a76e9b34433096a6c70
Roland Hartanto <13515107@std.stei.itb.ac.id>
2017-11-15 00:15:56 +0700

    recommend fixed 2

--- handler/recommend.py
+++ handler/recommend.py
@@ -11,13 +11,9 @@ class RecommendByPromoHandler(Handler):
   def __init__(self, reply_token, bot_api):
     carousel_template = CarouselTemplate(columns=[
 
-      CarouselColumn(text='Rp 20.000,-', title='Chinese Egg', actions=[
-        PostbackTemplateAction(label='Buy', data='buy_a'),
+      CarouselColumn(thumbnail_image_url='https://cdn.yoox.biz/46/46532237xt_14_f.jpg',text='Rp 1.320.000,- --> Rp 660.000,- (-50\%\off)', title='Camo Jacquard Straight Fit Jeans A|X', actions=[
+        PostbackTemplateAction(label='Buy', data='buy'),
         PostbackTemplateAction(label='Details', data='details_a')
-      ]),
-      CarouselColumn(text='Rp 15.000,-', title='Indonesian Egg', actions=[
-        PostbackTemplateAction(label='Buy', data='buy_b'),
-        PostbackTemplateAction(label='Details', data='details_b')
       ])
     ])
     template_message = TemplateSendMessage(
@@ -27,55 +23,22 @@ class RecommendByPromoHandler(Handler):
   def handle_postback(self, event, bot_api):
     data = event.postback.data
 
-    if data == 'details_a':
+    if data == 'buy':
+        self.switch_handler(PaymentHandler(event.reply_token, bot_api))
+    elif data == 'details_a':
       bot_api.reply_message(
-        event.reply_token, TextSendMessage(text="'Chinese egg\n\nOriginal Price: Rp. 25,000.00\nPrice after promo : Rp 20.000,-(Off 20%)\nStore location: Yogya Karapitan (Bandung)\nCondition: Good"))
-    elif data == 'details_b':
-      bot_api.reply_message(
-        event.reply_token, TextSendMessage(text="'Indonesian egg\n\nOriginal Price: Rp. 20,000.00\n\nPrice after promo : Rp 15.000,-(Off 25%)\nStore location: Yogya Riau Junction (Bandung)\nCondition: Good"))
-    elif data == 'compare':
-      self.switch_handler(CompareHandler(event.reply_token, bot_api))
+          event.reply_token, TextSendMessage(text='Camo Jacquard Straight Fit Jeans\nArmani Exchange\n\nPrice: Rp 1.320.000,- --> Rp 660.000,- (-50\%\off)\nStore location: Yogya Riau Junction (Bandung)\nCondition: Good'))
 
 class RecommendByHistoryHandler(Handler):
 
-  def __init__(self, reply_token, bot_api):
+ def __init__(self, reply_token, bot_api):
     carousel_template = CarouselTemplate(columns=[
 
-      CarouselColumn(text='Rp 19.500,-', title='Indonesian Egg', actions=[
-        PostbackTemplateAction(label='Buy', data='buy_a'),
-        PostbackTemplateAction(label='Details', data='details_a')
-      ]),
-      CarouselColumn(text='Rp 25.000,-', title='Arabian Egg', actions=[
-        PostbackTemplateAction(label='Buy', data='buy_b'),
-        PostbackTemplateAction(label='Details', data='details_b')
-      ])
-    ])
-    template_message = TemplateSendMessage(
-      alt_text='Carousel alt text', template=carousel_template)
-    bot_api.reply_message(reply_token, template_message)
-
-  def handle_postback(self, event, bot_api):
-    data = event.postback.data
-
-    if data == 'details_a':
-      bot_api.reply_message(
-        event.reply_token, TextSendMessage(text="'Indonesian egg\n\nPrice: Rp. 25,000.00\nYou've purchased this product 3 days ago\nStore location: Yogya Karapitan (Bandung)\nCondition: Good"))
-    elif data == 'details_b':
-      bot_api.reply_message(
-        event.reply_token, TextSendMessage(text="'Arabian egg\n\nPrice: Rp. 25,000.00\n\nStore location: Yogya Riau Junction (Bandung)\nCondition: Good"))
-    elif data == 'compare':
-      self.switch_handler(CompareHandler(event.reply_token, bot_api))
-
-class RecommendHandler(Handler):
-
-  def __init__(self, reply_token, bot_api):
-    carousel_template = CarouselTemplate(columns=[
-
-      CarouselColumn(text='Rp 25.000,-', title='Indonesian Egg', actions=[
+      CarouselColumn(thumbnail_image_url='https://cdn.yoox.biz/46/46539875vo_14_f.jpg',text='Rp 1.400.000,-', title='Skinny Patched Up Jeans A|X', actions=[
         PostbackTemplateAction(label='Buy', data='buy'),
         PostbackTemplateAction(label='Details', data='details_a')
       ]),
-      CarouselColumn(text='Rp 22.500,-', title='Japanese Egg', actions=[
+      CarouselColumn(thumbnail_image_url='https://matriposterous.files.wordpress.com/2010/11/image_298.jpg',text='Rp 25.000,-', title='Arabian Egg', actions=[
         PostbackTemplateAction(label='Buy', data='buy'),
         PostbackTemplateAction(label='Details', data='details_b')
       ])
@@ -88,10 +51,40 @@ class RecommendHandler(Handler):
     data = event.postback.data
 
     if data == 'buy':
-      self.switch_handler(PaymentHandler(event.reply_token, bot_api))
+        self.switch_handler(PaymentHandler(event.reply_token, bot_api))
     elif data == 'details_a':
       bot_api.reply_message(
-        event.reply_token, TextSendMessage(text="'Indonesian egg\n\nPrice: Rp. 25,000.00\nOrdered 5,454 times today\nStore location: Yogya Karapitan (Bandung)\nCondition: Good"))
+          event.reply_token, TextSendMessage(text='Skinny Patched Up Jeans\nArmani Exchange\n\nPrice: Rp 1.400.000,00\nStore location: Yogya Karapitan (Bandung)\nCondition: Good'))
     elif data == 'details_b':
       bot_api.reply_message(
-        event.reply_token, TextSendMessage(text="'Japanese egg\n\nPrice: Rp. 22,500.00\nOrdered 4,759 times today\nStore location: Yogya Sunda (Bandung)\nCondition: Good"))
+          event.reply_token, TextSendMessage(text='Arabian egg\n\nPrice: Rp. 25,000.00\nStore location: Yogya Karapitan (Bandung)\nCondition: Good'))
+
+class RecommendHandler(Handler):
+
+  def __init__(self, reply_token, bot_api):
+    carousel_template = CarouselTemplate(columns=[
+
+      CarouselColumn(thumbnail_image_url='https://cdn.yoox.biz/46/46539875vo_14_f.jpg',text='Rp 1.400.000,-', title='Skinny Patched Up Jeans A|X', actions=[
+        PostbackTemplateAction(label='Buy', data='buy'),
+        PostbackTemplateAction(label='Details', data='details_a')
+      ]),
+      CarouselColumn(thumbnail_image_url='https://cdn.yoox.biz/46/46532237xt_14_f.jpg',text='Rp 1.320.000,- --> Rp 660.000,- (-50\%\off)', title='Camo Jacquard Straight Fit Jeans A|X', actions=[
+        PostbackTemplateAction(label='Buy', data='buy'),
+        PostbackTemplateAction(label='Details', data='details_b')
+      ])
+    ])
+    template_message = TemplateSendMessage(
+      alt_text='Carousel alt text', template=carousel_template)
+    bot_api.reply_message(reply_token, template_message)
+
+  def handle_postback(self, event, bot_api):
+  data = event.postback.data
+
+  if data == 'buy':
+      self.switch_handler(PaymentHandler(event.reply_token, bot_api))
+  elif data == 'details_a':
+    bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='Skinny Patched Up Jeans\nArmani Exchange\n\nPrice: Rp 1.400.000,00\nStore location: Yogya Karapitan (Bandung)\nCondition: Good'))
+  elif data == 'details_b':
+    bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='Camo Jacquard Straight Fit Jeans\nArmani Exchange\n\nPrice: Rp 1.320.000,- --> Rp 660.000,- (-50\%\off)\nStore location: Yogya Riau Junction (Bandung)\nCondition: Good'))
