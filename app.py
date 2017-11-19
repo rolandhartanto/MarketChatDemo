b3821520baefcfa80cb3f5ca037906c523209330
Roland Hartanto <13515107@std.stei.itb.ac.id>
2017-11-19 14:25:34 +0700

    try to delete are you lost message in app.py

--- app.py
+++ app.py
@@ -109,11 +109,11 @@ def handle_text_message(event):
       event.reply_token,
       TextMessage(text='Type "menu" to view main menu.\nTo view other instructions, type "help".'))
     state.switch_handler(DefaultHandler())
-  else:
-  	bot_api.reply_message(
-      event.reply_token,
-      TextMessage(text='Are you lost?\nPlease type "done" or "cancel"'))
-  	state.switch_handler(DefaultHandler())
+  #else:
+  #	bot_api.reply_message(
+  #    event.reply_token,
+  #    TextMessage(text='Are you lost?\nPlease type "done" or "cancel"'))
+  #	state.switch_handler(DefaultHandler())
 
   state.handle_text(event, bot_api)
 
