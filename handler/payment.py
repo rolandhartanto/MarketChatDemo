58aa375769ef3e968b57090ba0a132f833e6300e
Agus Gunawan <13515143@std.stei.itb.ac.id>
2017-11-30 22:01:06 +0700

    Fix Transfer Payment

--- handler/payment.py
+++ handler/payment.py
@@ -20,9 +20,15 @@ class PaymentHandler(Handler):
     data = event.postback.data
 
     if data == 'transfer':
+      text1 = TextMessage(text='Wait a moment while we validate your transactions\'s seller status.')
+      text2 = TextMessage(text='We already validate your transaction\'s seller status.')
+      text3 = TextMessage(text='Your seller status verdict are safe.')
+      text4 = TextMessage(text='The seller account number are: 900-00-123-123.')
+      text5 = TextMessage(text='We guarantee that transfer payment with the seller is safe.')
+      text6 = TextMessage(text='If you have any dificulty in the payment please contact our administrator: 082821821821.')
       bot_api.reply_message(
         event.reply_token,
-        TextMessage(text='Here\'s your seller account number: 900-00-123-123\n\nYour seller are certified seller\n\nType "validate transfer" to validate your transfer evidence.'))
+        [text1, text2, text3, text4, text5, text6])
     elif data == 'cod':
       buttons_template = ButtonsTemplate(
         title='When?', text='Choose schedule:', actions=[
