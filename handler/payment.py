44f128495fabcceef1131b9226ca0932613e4f44
Agus Gunawan <13515143@std.stei.itb.ac.id>
2017-12-01 09:23:20 +0700

    try to fix cod

--- handler/payment.py
+++ handler/payment.py
@@ -27,8 +27,8 @@ class PaymentHandler(Handler):
     elif data == 'cod':
       buttons_template = ButtonsTemplate(
         title='When?', text='Choose Schedule:', actions=[
-          PostbackTemplateAction(label='Time - Place: 30 Nov 08.00 - Marina', data='choose1'),
-          PostbackTemplateAction(label='Time - Place: 20 Dec 19.00 - Sydney', data='choose2')
+          PostbackTemplateAction(label='30 Nov 08.00 - Marina', data='choose1'),
+          PostbackTemplateAction(label='20 Dec 19.00 - Sydney', data='choose2')
         ])
       template_message = TemplateSendMessage(
         alt_text='Cash On Delivery Payment', template=buttons_template)
